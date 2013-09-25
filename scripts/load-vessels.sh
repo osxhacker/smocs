23e311b6f120f540217be4a7b9f51227253aa1b4
Steve Vickers <steve.b.vickers@gmail.com>
2013-09-25 17:01:55 -0400

    Migrated load-vessels.sh to use the start script

--- scripts/load-vessels.sh
+++ scripts/load-vessels.sh
@@ -5,12 +5,16 @@
 # execute from the smocs-examples directory
 cd $(dirname $0)/..
 
-typeset -r BASE_DIR="$(cd ..; pwd)"
+typeset -r BASE_DIR="$PWD"
 
 test -d smocs-examples/target/scala-2.10/classes || {
 	echo "the project must be built before running ${0}" >&2
 	exit 1
 	}
 
-sbt "project smocs-examples" \
-	"run com.tubros.constraints.examples.cargo.LoadVessels $*"
+export JAVA_OPTS="-Xmx2048M -XX:MaxPermSize=512M -Xss2M -XX:ReservedCodeCacheSize=64m -XX:+UseCodeCacheFlushing -XX:+UseConcMarkSweepGC -Duser.timezone=GMT"
+
+sbt "project smocs-examples" start-script && \
+	./smocs-examples/target/start \
+	com.tubros.constraints.examples.cargo.LoadVessels \
+	$*
